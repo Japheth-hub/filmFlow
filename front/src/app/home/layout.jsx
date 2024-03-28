4751a376d78158fac1d838a74abc395dab8d43b5
NDelPozo <nickdelpo@gmail.com>
2024-03-28 16:32:23 -0300

    test search

--- front/src/app/home/layout.jsx
+++ front/src/app/home/layout.jsx
@@ -2,27 +2,15 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
-import { Provider } from 'react-redux'
 
 export default function layout({children}) {
   return (
-<<<<<<< HEAD
-    <div>
-      <Provider>
-        
-=======
     <>  
->>>>>>> cc4d357b8cade6561af20e31a13ade287cda1f22
       <Navbar />
       <div className="wrapper">
         {children}
       </div>
       <Footer />
-<<<<<<< HEAD
-      </Provider>
-    </div>
-=======
     </>
->>>>>>> cc4d357b8cade6561af20e31a13ade287cda1f22
   )
 }
