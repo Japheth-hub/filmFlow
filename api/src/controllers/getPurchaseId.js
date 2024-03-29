5fa7499db2d2aa1eadf7afe429b81568441ba61f
Japheth Ramirez <japheth.ra369@gmail.com>
2024-03-28 19:52:04 -0600

    Creando ruta para obtener las peliculas compradas por usuario

---
+++ api/src/controllers/getPurchaseId.js
@@ -0,0 +1,27 @@
+const { User, Movie } = require("../db");
+
+module.exports = async (req) => {
+    try {
+        const user = req.user
+        const userId = user.id
+        const usuario = await User.findByPk(userId,{
+            include: [{ model: Movie }],
+        });
+        if(usuario && usuario.movies.length > 0){
+            const peliculas = []
+            console.log('este es el user', usuario.movies)
+            for(let movie of usuario.movies){
+                const pelicula = await Movie.findByPk(movie.id, {
+                    attributes: ['id', 'name', 'poster']
+                })
+                peliculas.push(pelicula)
+            }
+            return peliculas
+        } else {
+            return 'Este usuario aun no tiene compras'
+        }
+        
+    } catch (error) {
+        return error
+    }
+}
