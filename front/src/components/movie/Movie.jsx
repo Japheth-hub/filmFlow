342e245a62d0a858a3579f03044e0dca75e57972
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-04-04 11:08:33 -0600

    logica de película comprada

--- front/src/components/movie/Movie.jsx
+++ front/src/components/movie/Movie.jsx
@@ -27,7 +27,7 @@ const Movie = ({ elem, dim }) => {
                     const {data} = await axios(`${URL}purchases/${user.sid}`)
                     if(typeof data === "object"){
                         const idsMovies = []
-                        for(let movie of data.movies){
+                        for(let movie of data){
                             idsMovies.push(movie.id)
                         }
                         setPurchase(idsMovies)
