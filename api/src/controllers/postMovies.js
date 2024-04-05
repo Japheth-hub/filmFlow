af8fb2b11e6d55f301dea89fe8a0714510045be5
Baidewei <gerant9seminario@gmail.com>
2024-04-05 09:54:11 -0400

    Hotfix de postMovies en el back

--- api/src/controllers/postMovies.js
+++ api/src/controllers/postMovies.js
@@ -25,10 +25,7 @@ module.exports = async (req) => {
            return {status:false,errors:validation.errors}
         }
 
-        let { name, director, genres, description, country, posterFile, trailerFile, movieFile} = body;
-        let countryCode = country
-       
-        const status = "pending" 
+        let { name, director, genres, description, countries, posterFile, trailerFile, movieFile} = body;
 
         //Cloudinary:
         //Convertir el archivo a buffer de bytes para que Cloudinary sea capaz de leerlo (Los .mp4 no se convierten a buffer)
@@ -78,17 +75,13 @@ module.exports = async (req) => {
         //Provisional mientras no existen estos inputs en el formulario
         const price = 25
         const year = 2020
+        //
+
+        const status = "pending" 
         
         const userId = isAdmin(user) ? undefined : user.id;
+
         const [movieDB, created] = await Movie.findOrCreate({
-            include: [{
-                model: Country,
-                required: true,
-                attributes: [],
-                where: {
-                    id: countryCode 
-                }
-            }],
             where: { 
                 name: name,
                 director: director,
@@ -121,10 +114,14 @@ module.exports = async (req) => {
                     movieDB.addGenre(genreDB);
                 }
             }
-            const countryDB = await Country.findByPk(countryCode)
 
-            if(countryDB){
-                movieDB.addCountry(countryDB)
+            countries = countries.split(',').map((item) => item.trim());
+            for (countryCode of countries) {
+                const countryDB = await Country.findByPk(countryCode)
+                
+                if(countryDB){
+                    movieDB.addCountry(countryDB)
+                }
             }
         }
 
@@ -133,8 +130,9 @@ module.exports = async (req) => {
         }
 
         //Prueba para el envio de mails
+        console.log(user)
         const mailInfo = {
-            destination: `gerant9seminario@gmail.com`,
+            destination: `${user.email}`,
             topic: "Pelicula creada con exito",
             content: `Se ha creado su pelicula: ${name} exitosamente`,
         };
