c1048c3b5ba24cb23973d57d636e9e9d9fdbd5b7
unknown <julian.f.vega@gmail.com>
2024-04-06 10:43:47 -0300

    UPD: dashboard Graphics

--- front/src/components/dashGrap/dashArea/DashArea.jsx
+++ front/src/components/dashGrap/dashArea/DashArea.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import style from '../DashGrap.module.scss';
 import {Line} from "react-chartjs-2";
 
-export default function DashArea({chartData}) {
+export default function DashArea({chartData, title}) {
     return (   
-            <div className={style.content}><Line data={chartData} /></div>
+            <div>
+                <h3>{title}</h3>
+                <div className={style.content}><Line data={chartData} /></div>
+            </div>
     );
 }
