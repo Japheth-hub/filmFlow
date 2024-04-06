69df3a563914f7c1a4d1486b6e8b6d045c71248a
Baidewei <gerant9seminario@gmail.com>
2024-04-05 21:26:37 -0400

    devueltos los putUsers viejos

--- api/src/handlers/putUserHandler.js
+++ api/src/handlers/putUserHandler.js
@@ -19,3 +19,28 @@ module.exports = async (req, res) => {
         return res.status(500).json(error)
     }
 }
+
+//Viejo putUserHandler:
+// const putUser = require('../controllers/putUser')
+
+// module.exports = async (req, res) => {
+//     try {
+//         const {id} = req.params
+//         const body = req.body
+
+//         if(Object.keys(body).length === 0){
+//             return res.status(400).json({message: 'No hay datos para actualizar'})   
+//         }
+
+//         const data = await putUser(id, body)
+
+//         if(!data.status){
+//             return res.status(404).json(data)
+//         }
+        
+//         return res.status(200).json(data)
+
+//     } catch (error) {
+//         return res.status(500).json(error)
+//     }
+// }
