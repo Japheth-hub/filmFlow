f7ced9aae0529955081cde15e733882af967c001
Baidewei <gerant9seminario@gmail.com>
2024-04-05 21:23:00 -0400

    Actualizar roles de usuarios

--- api/src/controllers/putUser.js
+++ api/src/controllers/putUser.js
@@ -1,18 +1,38 @@
-const { User } = require('../db')
+const { User, Role } = require('../db');
 
-module.exports = async (id, body) => {
+module.exports = async (body) => {
     try {
-        const data = {}
-        const user = await User.findOne({where : {id}})
-        if(user){
-            for(let key in body){
-                if(key!==  "auth") user[key] = body[key] ? body[key] : user[key]
-            }
-            await user.save();
-            return {status:true,user};
+        const { userSid, roleToChange } = body;
+
+        const user = await User.findOne({
+            where: { sid: userSid }
+        });
+        if (!user) {
+            return { status: false, message: 'Error fetching user' };
         }
-        return {status:false,message:'No existe pelicula con ese Id'} 
+
+        const producerRole = await Role.findOne({ where: { role: "producer" } });
+        const adminRole = await Role.findOne({ where: { role: "admin" } });
+
+        if (user.roleId === producerRole.id && roleToChange !== "admin") {
+            return { status: false, message: 'The user already is producer' };
+        }
+        if (user.roleId === adminRole.id && roleToChange !== "producer") {
+            return { status: false, message: 'The user already is admin' };
+        }
+
+        if (roleToChange === 'admin') {
+            user.roleId = adminRole.id;
+            await user.save();
+            return { status: true, message: 'Successfully changed the user to admin' };
+        }
+        if (roleToChange === 'producer') {
+            user.roleId = producerRole.id;
+            await user.save();
+            return { status: true, message: 'Successfully changed the user to producer' };
+        }
     } catch (error) {
-        return {status:false,error} 
+        console.log(error);
+        return { status: false, error };
     }
-}
+};
--- api/src/handlers/putUserHandler.js
+++ api/src/handlers/putUserHandler.js
@@ -2,21 +2,19 @@ const putUser = require('../controllers/putUser')
 
 module.exports = async (req, res) => {
     try {
-        const {id} = req.params
         const body = req.body
 
         if(Object.keys(body).length === 0){
             return res.status(400).json({message: 'No hay datos para actualizar'})   
         }
 
-        const data = await putUser(id, body)
+        const data = await putUser(body)
 
         if(!data.status){
             return res.status(404).json(data)
         }
         
         return res.status(200).json(data)
-
     } catch (error) {
         return res.status(500).json(error)
     }
