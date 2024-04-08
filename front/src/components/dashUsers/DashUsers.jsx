651063d5298bb079541661fb9dbc2b4b27705571
Oyuco <marcos132625@gmail.com>
2024-04-08 01:03:22 -0400

    Agrega de alertas y pequeños estilos

--- front/src/components/dashUsers/DashUsers.jsx
+++ front/src/components/dashUsers/DashUsers.jsx
@@ -3,6 +3,7 @@ import style from '../../app/admin/admin.module.scss'
 import Button from '../button/Button'
 import axios from 'axios'
 import { useUser } from '@auth0/nextjs-auth0/client'; 
+import Swal from 'sweetalert2'
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL
 
 
@@ -10,46 +11,83 @@ export default function DashUsers({datos, link}) {
     const [column, setColumn] = useState([])
     const [body, setBody] = useState([])
     const [order, setOrder] = useState(true)
+    // const [orderByRole, setOrderByRole] = useState(false);
 
     const {user} = useUser();
 
     async function rolChange(sid, rolToChange){
         try {
             if (user.sid === sid) {
-                return alert('No puedes cambiar tu propio rol')
+                return Swal.fire({
+                    icon: 'warning',
+                    title: '¡Advertencia!',
+                    text: 'No puedes cambiar tu propio rol',
+                  });
             }
             
-            const res = confirm(`Estas seguro que deseas convertir a ese usuario en ${rolToChange}`)
-            if(res){
+            const res = await Swal.fire({
+                icon: 'question',
+                title: '¿Estás seguro?',
+                text: `Estas seguro que deseas convertir a ese usuario en ${rolToChange}`,
+                showCancelButton: true,
+                confirmButtonText: 'Sí',
+                cancelButtonText: 'Cancelar',
+              });
+          
+                if (res.isConfirmed) {        
                 const rolUpdateInfo = {
-                            "auth": `${user.sid}`,
-                            "userSid": `${sid}`,
-                            "roleToChange": `${rolToChange}`
-                            }
+                "auth": `${user.sid}`,
+                "userSid": `${sid}`,
+                "roleToChange": `${rolToChange}`
+                };
                 try {
                     const rolUpdateResponse = await axios.put(`${link}`, rolUpdateInfo);
-                    alert(rolUpdateResponse.data.message);
-                    window.location.reload()
+                    Swal.fire({
+                        icon: 'success',
+                        title: '¡Éxito!',
+                        text: rolUpdateResponse.data.message,
+                    });
+                    const updatedBody = body.map((item) => {
+                        if (item.sid === sid) {
+                        return { ...item, role: rolToChange };
+                        }
+                        return item;
+                    });
+                    setBody(updatedBody);
                 } catch (error) {
-                    alert(error.response.data.message);
+                    Swal.fire({
+                        icon: 'error',
+                        title: '¡Error!',
+                        text: error.response.data.message,
+                      });
                 }
             }
         } catch (error) {
             console.log(error)
         }
     }
-
-    function handleOrder(tipo){
-        setOrder(!order)
-        if(tipo === 'Name'){
-            if(!order){
-                body.sort((a, b) => a.name.localeCompare(b.name));
-            } else {
-                body.sort((a, b) => b.name.localeCompare(a.name));
-            }
+    function handleOrder(tipo) {
+        let sortedBody = [...body]; // Crear una copia del estado actual de body
+    
+        if (tipo === 'Name') {
+          setOrder(!order);
+          if (!order) {
+            sortedBody.sort((a, b) => a.name.localeCompare(b.name));
+          } else {
+            sortedBody.sort((a, b) => b.name.localeCompare(a.name));
+          }
+        } else if (tipo === 'Role') {
+          setOrder(!order);
+          if (!order) {
+            sortedBody.sort((a, b) => a.role.localeCompare(b.role));
+          } else {
+            sortedBody.sort((a, b) => b.role.localeCompare(a.role));
+          }
         }
-    }
-
+    
+        setBody(sortedBody); // Actualizar el estado de body con el nuevo orden
+      }
+    
     useEffect(()=>{
         function getData(datos){
             if(datos && datos.length > 0){
@@ -68,6 +106,8 @@ export default function DashUsers({datos, link}) {
     <div>
         <div className={style.orderFilters}>
             <Button callback={()=>{handleOrder('Name')}} emoji={order ? '🔻' : '🔺'} label={'Name'}></Button>
+            <Button callback={()=>{handleOrder('Role')}} emoji={order ? '🔻' : '🔺'} label={'Role'}></Button>
+            
         </div>
 
         <div className={style.divTabla}>
