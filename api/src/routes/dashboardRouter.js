9f3b8b40957ab87c32b237ac11fc152a37148a1d
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-04-11 15:32:43 -0600

    run report route

--- api/src/routes/dashboardRouter.js
+++ api/src/routes/dashboardRouter.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const getDashboardHandler = require('../handlers/getDashboardHandler');
+const postProducerReportCron = require('../controllers/postProducerReportCron');
+
 
 const getCheckAuth = require("../middlewares/getCheckAuth");
 const dashboardRouter = Router();
 
 
 dashboardRouter.get('/:auth', getCheckAuth, getDashboardHandler);
+dashboardRouter.get('/report/', getCheckAdmin, postProducerReportCron.dailyJob());
 
 module.exports = dashboardRouter;
