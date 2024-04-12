df4f640de1349aa170627a84a07c55cefcde7ef4
Japheth Ramirez <japheth.ra369@gmail.com>
2024-04-12 09:47:33 -0600

    Correcion en detail

--- front/src/app/detail/[id]/page.jsx
+++ front/src/app/detail/[id]/page.jsx
@@ -38,7 +38,6 @@ const DetailContent = () => {
   const [idReview, setIdReview] = useState()
   const [alerts, setAlerts] = useState({points: true, comment: true})
 
-
   const goToCategory = (genre) => {
     router.push(`/movies?genre=${genre}`);
   };
@@ -95,7 +94,6 @@ const DetailContent = () => {
         setIsLoading(false);
       }
     };
-
     fetchData();
   }, [id]);
 
@@ -133,9 +131,13 @@ const DetailContent = () => {
         console.error('Error fetching purchase data:', error);
       }
     }
-    getPurchase()
-    setReview(reviewsData.find((review) => review.user.email ? review.user.email === user.email : review.user.name === user.email))
-    }, [reviewsData])
+
+    if(user){
+      getPurchase()
+    }
+  }, [user])
+  
+
 
   const toggleMediaType = () => {
     setMediaType(prevMediaType => prevMediaType === 'trailer' ? 'movie' : 'trailer');
@@ -166,8 +168,9 @@ const DetailContent = () => {
     year,
   } = movieData;
 
-  const country = countries.map(country => country.name.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '));
+  // const country = countries.map(country => country.name.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '));
 
+  const country = ""; 
   const handleRatingChange = (rating) => {
     setNewReview({ ...newReview, points: rating });
   };
@@ -266,7 +269,7 @@ const renderStarSelector = () => {
           <iframe src={movie} width="800" height="500" title="Movie" allowFullScreen />
         )}
       </div>  
-      {purchase.includes(movieData.id) && !review 
+      {purchase.includes(movieData.id) && !review || purchase.includes(movieData.id) && reviewsData.length === 0
         ? <div className={style['review-form-container']}>
             <h4>Deja un comentario</h4>
             {successMessage && <div className={style['success-message']}>{successMessage}</div>}
@@ -334,5 +337,3 @@ const renderStarSelector = () => {
 };
 
 export default DetailContent;
-
-
