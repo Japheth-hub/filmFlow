02b660bdda51f15b9eb078d9fa46bce444ef5703
Baidewei <gerant9seminario@gmail.com>
2024-04-15 17:42:35 -0400

    Fix cron & intento de fix del error de home

---
+++ api/src/controllers/getPurchasesDashboard.js
@@ -0,0 +1,48 @@
+const { Purchase,Movie,PurchaseMovie } = require('../db');
+const { Op } = require('sequelize');
+
+module.exports = async function getPurchases(req){
+    try {
+        const {limit,user,month} = req.query;
+        
+        let options = {};
+        
+        if(limit){    
+            options = {
+                ...options,
+                limit
+            }
+        }
+
+        if(user){
+            options.include =  [
+                {
+                model: Movie,
+                where: { userId: user },
+                through: {
+                    model: PurchaseMovie,
+                    attributes: ['price'], 
+                },
+                }
+            ]
+        }
+
+        if(month){
+            const endDate = new Date();
+            const startDate = new Date(endDate.getFullYear(), endDate.getMonth(), 1);
+            options.where =  {
+                createdAt: {
+                    [Op.between]: [startDate, endDate]
+                }
+            }
+        }
+
+
+        const purchases = await Purchase.findAll({...options});
+
+        return { status: true, purchases: purchases}
+    } catch (error) {
+        console.error(error)
+        return { status: false, message: "Error:", error}
+    }
+}
--- api/src/handlers/getPurchaseHandler.js
+++ api/src/handlers/getPurchaseHandler.js
@@ -1,14 +1,14 @@
-const getPurchases = require('../controllers/getPurchases')
+const getPurchasesDashboard = require('../controllers/getPurchasesDashboard')
 
 module.exports = async (req, res) => {
     try {
-        const data = await getPurchases(req)
+        const data = await getPurchasesDashboard(req)
 
         if(!data.status) {
-            return res.status(422).json(data.message)
+            return res.status(422).json(data)
         }
 
-        return res.status(200).json(data.purchases)
+        return res.status(200).json(data)
     } catch (error) {
         console.error(error)
         return res.status(500).json(error)
